a30938fa4c1a64d32def95f714c45fbb69d05df7
Mohammad Abuzar <mohammad.abuzar@unthinkable.co>
2023-09-17 21:44:16 +0530

    I have added nav bar and created dark banner page

--- wp-content/themes/CodeCraft/header.php
+++ wp-content/themes/CodeCraft/header.php
@@ -7,12 +7,28 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<header>
+<header class="ontainer">
     <div class="logo">
-        <a href="<?php echo esc_url(home_url('/')); ?>">
-            <img src="<?php echo get_template_directory_uri(); ?>/logo.png" alt="Your Website Logo">
-        </a>
+        <?php
+            $site_title = get_bloginfo('name');
+            $words = explode(' ', $site_title); // Split the site title into words
+
+            if (count($words) > 0) {
+                $first_word = $words[0]; // Get the first word
+                $remaining_words = implode(' ', array_slice($words, 1)); // Get the remaining words
+
+                // Output the first word with a specific CSS class for styling
+                echo '<a href="' . esc_url(home_url('/')) . '"><span class="first-word">' . esc_html($first_word) . '</span></a>';
+
+                // Output the remaining words
+                echo esc_html($remaining_words);
+            } else {
+                // Output the site title as is
+                echo esc_html($site_title);
+            }
+        ?>
     </div>
+
     <nav>
     <?php
         wp_nav_menu(array(
@@ -22,4 +38,15 @@
     ?>
 
     </nav>
+    <div class="buttons">
+        <div class="nav-button">
+            Let's Talk
+        </div>
+        <input type="checkbox" id="darkModeToggle">
+        <label for="darkModeToggle"></label>
+    </div>
+
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    
 </header>
+<?php wp_footer(); ?>
--- wp-content/themes/CodeCraft/page.php
+++ wp-content/themes/CodeCraft/page.php
@@ -2,3 +2,53 @@
 
 get_header();
 ?>
+<div class="continer main-content">
+    <div class="text-container">
+        <div class="banner-title">
+            <?php if ( $banner_title = get_field( 'banner_title' ) ) : ?>
+                <?php echo esc_html( $banner_title ); ?>
+            <?php endif; ?>
+        </div>
+
+        <div class="banner-content">
+            <?php if ( $banner_content = get_field( 'banner_content' ) ) : ?>
+                <?php echo esc_html( $banner_content ); ?>
+            <?php endif; ?>
+        </div>
+
+        <div class="buttons">
+            <div class="project-button">
+                <?php
+                $link = get_field( 'project_button' );
+                if ( $link ) :
+                    $link_url = $link['url'];
+                    $link_title = $link['title'];
+                    $link_target = $link['target'] ? $link['target'] : '_self';
+                    ?>
+                    <a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+                <?php endif; ?>
+            </div>
+
+            <div class="video-button">
+                <?php
+                $link = get_field( 'video_button' );
+                if ( $link ) :
+                    $link_url = $link['url'];
+                    $link_title = $link['title'];
+                    $link_target = $link['target'] ? $link['target'] : '_self';
+                    ?>
+                    <a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+    <div class="image-container">
+        <div class="banner-image">
+        <?php
+            $banner_image = get_field( 'banner_image' );
+            if ( $banner_image ) : ?>
+                <img src="<?php echo esc_url( $banner_image['url'] ); ?>" alt="<?php echo esc_attr( $banner_image['alt'] ); ?>" />
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
